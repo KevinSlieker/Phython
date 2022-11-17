c0358833792db80415a54cbfb52f7a5d70c6d2e0
Kevin Slieker <kevin.slieker.10@gmail.com>
2022-11-17 15:52:40 +0100

    Opdrachten

---
+++ extra_opdracht.py
@@ -0,0 +1,39 @@
+name = ("Jeroen Heemskerk", "Slieker, Kevin", "de Groot, Henk", "Arje van der Geest", "'O Brien, Willem Anton Stuward", "Jan Willem Klaasen")
+infix_array = ["van", "der", "'O"]  # other infixes to add
+data_base = []
+for person in name:
+    first_name = ""
+    last_name = ""
+    infix = ""
+    other_names = ""
+    if ',' not in person:
+        parts = person.split()
+        first_name = parts[0]
+        last_name = parts[-1]
+        for between in parts[1:-1]:
+            if between in infix_array:
+                infix += " " + between
+            else:
+                other_names += " " + between
+    else:
+        parts = person.split(",")
+        before_com = parts[0]
+        after_com = parts[1]
+        before = before_com.split()
+        after = after_com.split()
+        first_name = after[0]
+        last_name = before[-1]
+        if len(before) > 1:
+            for i in before[0:-1]:
+                infix += " " + i
+        if len(after) > 1:
+            for o in after[1:]:
+                other_names += " " + o
+
+    data_base.append({"First name" : first_name, "Other names" : other_names.strip(), "Infix" : infix.strip(), "Last Name" : last_name})
+print(data_base)
+
+
+# for person in data_base:
+#     if person["First name"] == "Jeroen":
+#         print(person)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,29 +1,22 @@
-board_size = 10
+name = ("Jeroen Heemskerk", "Slieker, Kevin", "de Groot, Henk", "Arje van der Geest", "'O Brien, Willem Anton Stuward", "Jan Willem Klaasen")
+print(name)
+print(type(name))
+print(len(name))
+data_base = [{"First name" : "", "Other names" : "", "Infix" : "", "Last Name" : ""}]
+print(data_base)
+print(type(data_base))
+data_base.append({"First name" : "a", "Other names" : "b", "Infix" : "c", "Last Name" : "d"})
+print(data_base)
+print(name[1].split(sep=","))
+print(len(name[1].split()))
 
-board = [[[] for row in range(board_size)] for  column in range(board_size)]
-number = 1
-for j in range(0,board_size):
-    for i in range(0,board_size):
-        board [j][i] = number
-        number+=1
-# board [0] [0] = 1
-# board [0] [1] = 2
-# board [0] [2] = 3
-# board [1] [0] = 4
-board [1] [1] = 'X'
-# board [1] [2] = 6
-# board [2] [0] = 7
-# board [2] [1] = 8
-# board [2] [2] = 9
-
-
-
-print("+", "-------+"*board_size, sep='')
-for j in range(0,board_size):
-    print("|", "       |"*board_size, sep='')
-    print("|", sep='', end="")
-    for i in range(0,board_size-1):
-        print(str(board [j] [i]).center(7," "),"|", sep='', end="")
-    print(str(board [j] [board_size-1]).center(7," "),"|", sep='')
-    print("|", "       |"*board_size, sep='')
-    print("+", "-------+"*board_size, sep='')
+for person in name:
+    print(person)
+    print(type(person))
+    infix = ""
+    if ',' not in person:
+        parts = person.split()
+        first_name = parts[0]
+        print(first_name)
+        last_name = parts[-1]
+        print(last_name)
